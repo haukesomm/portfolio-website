7561676b7b67f8627fdd351f7cc25be85e15f008
Hauke Sommerfeld <development@haukesomm.de>
2024-01-22 23:03:47 +0100

    Adjust project overview style on medium screen sizes

--- src/lib/components/ProjectOverview.svelte
+++ src/lib/components/ProjectOverview.svelte
@@ -5,7 +5,7 @@
     export let userWithPinnedRepos: UserWithPinnedRepos;
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
     {#each userWithPinnedRepos.pinnedItems.nodes as repo}
         <ProjectCard
             username={userWithPinnedRepos.login}
